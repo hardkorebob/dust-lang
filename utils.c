1f09090e2a631020f9daddf9fcdb8a6bc487cd56
Roberto Rodriguez Jr <64380209+hardkorebob@users.noreply.github.com>
2025-09-07 10:04:57 -0400

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,4 +1,3 @@
-// ===== utils.c =====
 #include <string.h>
 #include <stdlib.h>
 
@@ -6,7 +5,7 @@ char* clone_string(const char* str) {
     if (!str) return NULL;
     size_t len = strlen(str);
     char* new_str = malloc(len + 1);
-    if (!new_str) return NULL; // Always check if malloc succeeded
+    if (!new_str) return NULL; 
     memcpy(new_str, str, len);
     new_str[len] = '\0';
     return new_str;
